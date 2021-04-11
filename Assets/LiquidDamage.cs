40c2c0b78a2a45f8cf03681f396f516554141d28
Elias999-creator <71507603+Elias999-creator@users.noreply.github.com>
2021-04-11 17:30:14 +0100

    Implemented liquid damage

---
+++ Assets/LiquidDamage.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class LiquidDamage : MonoBehaviour
+{
+    void OnTriggerEnter(Collider collision)
+    {
+        var Player = collision.GetComponent<PlayerController>();
+        if (Player)
+        {
+            PlayerManager.instance.KillPlayer();
+        }
+    }
+}
