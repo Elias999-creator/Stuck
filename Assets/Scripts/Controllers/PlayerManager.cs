fdfc964afc0e53a1392df621bd90c5be450f3a66
Elias999-creator <71507603+Elias999-creator@users.noreply.github.com>
2021-04-25 23:04:14 +0100

    Created it so the music changes when you're in and out of combat

--- Assets/Scripts/Controllers/PlayerManager.cs
+++ Assets/Scripts/Controllers/PlayerManager.cs
@@ -11,12 +11,33 @@ public class PlayerManager : MonoBehaviour
 
     void Awake()
     {
-        instance = this;    
+        instance = this;
     }
 
     #endregion
 
     public GameObject player;
+    GameObject[] enemies;
+    bool inBattle;
+
+    private void LateUpdate()
+    {
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        inBattle = false;
+        foreach (var enemy in enemies)
+        {
+            float distance = Vector3.Distance(enemy.transform.position, player.transform.position);
+            if (distance <= 10f) {
+                inBattle = true;
+                break;
+            }
+        }
+
+        if (inBattle)
+            MusicManager.instance.ChangeMusic(MusicManager.instance.battleMusic);
+        else
+            MusicManager.instance.ChangeMusic(MusicManager.instance.overworldMusic);
+    }
 
     public void KillPlayer ()
     {
---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    #region Singleton
+    public static MusicManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    #endregion
+
+    AudioSource audioSource;
+    public AudioClip overworldMusic;
+    public AudioClip battleMusic;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void ChangeMusic(AudioClip newMusic)
+    {
+        if (audioSource.clip != newMusic)
+        {
+            audioSource.clip = newMusic;
+            audioSource.Play();
+        }
+    }
+}
