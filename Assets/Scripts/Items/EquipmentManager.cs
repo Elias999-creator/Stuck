37f101526c7896febe701148b9f5c05c0783c91e
Elias999-creator <71507603+Elias999-creator@users.noreply.github.com>
2021-04-29 19:50:38 +0100

    Removed equipment meshes

--- Assets/Scripts/Items/EquipmentManager.cs
+++ Assets/Scripts/Items/EquipmentManager.cs
@@ -16,9 +16,7 @@ public class EquipmentManager : MonoBehaviour
     #endregion
 
     public Equipment[] defaultItems;
-    public SkinnedMeshRenderer targetMesh;
     Equipment[] currentEquipment;
-    SkinnedMeshRenderer[] currentMeshes;
 
     public delegate void OnEquipmentChanged(Equipment newItem, Equipment oldItem);
     public OnEquipmentChanged onEquipmentChanged;
@@ -31,7 +29,6 @@ public class EquipmentManager : MonoBehaviour
 
         int numSlots = System.Enum.GetNames(typeof(EquipmentSlot)).Length;
         currentEquipment = new Equipment[numSlots];
-        currentMeshes = new SkinnedMeshRenderer[numSlots];
 
         EquipDefaultItems();
     }
@@ -46,27 +43,14 @@ public class EquipmentManager : MonoBehaviour
             onEquipmentChanged.Invoke(newItem, oldItem);
         }
 
-        SetEquipmentBlendShapes(newItem, 100);
-
         currentEquipment[slotIndex] = newItem;
-        SkinnedMeshRenderer newMesh = Instantiate<SkinnedMeshRenderer>(newItem.mesh);
-        newMesh.transform.parent = targetMesh.transform;
-
-        newMesh.bones = targetMesh.bones;
-        newMesh.rootBone = targetMesh.rootBone;
-        currentMeshes[slotIndex] = newMesh;
     }
 
     public Equipment Unequip(int slotIndex)
     {
         if (currentEquipment[slotIndex] != null)
         {
-            if(currentMeshes[slotIndex] != null)
-            {
-                Destroy(currentMeshes[slotIndex].gameObject);
-            }
             Equipment oldItem = currentEquipment[slotIndex];
-            SetEquipmentBlendShapes(oldItem, 0);
             inventory.Add(oldItem);
 
             currentEquipment[slotIndex] = null;
@@ -87,14 +71,6 @@ public class EquipmentManager : MonoBehaviour
             Unequip(i);
         }
         EquipDefaultItems();
-    }
-
-    void SetEquipmentBlendShapes(Equipment item, int weight)
-    {
-        foreach (EquipmentMeshRegion blendShape in item.coveredMeshRegions)
-        {
-            targetMesh.SetBlendShapeWeight((int)blendShape, weight);
-        }
     }
 
     void EquipDefaultItems()
