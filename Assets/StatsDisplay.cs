5fd1fdeed0a298478a85c459bf6797440fd2b39f
Elias999-creator <71507603+Elias999-creator@users.noreply.github.com>
2021-05-03 20:54:14 +0100

    Created base of having the stats display on the screen

---
+++ Assets/StatsDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StatsDisplay : MonoBehaviour
+{
+    PlayerStats stats;
+    TextMeshProUGUI text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        stats = PlayerManager.instance.player.GetComponent<PlayerStats>();
+        text = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = $"+{stats.damage.GetValue()} damage\n+{stats.armor.GetValue()} armor";
+    }
+}
